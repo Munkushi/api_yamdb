fc9a3059083951bb85b669e98f6009997dddf88b
Даниил Цыганов <daniil.tsiganov@gmail.com>
2022-03-14 20:26:20 +0300

    second merge

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+
+class Titles(models.Model):
+    """Модель названий."""
+    category = models.ForeignKey()
+    genre = models.ForeignKey()
+    name = models.TextField()
+    year = models.CharField()
+
+
+class Genres(models.Model):
+    """Модель жанров."""
+    slug = models.SlugField()
+
+
+class Categories(models.Model):
+    """Модель категорий."""
+    slug = models.Model
+    name = models.TextField
