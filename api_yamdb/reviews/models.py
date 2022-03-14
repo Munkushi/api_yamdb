05514bed107849d9a8c2ffffe6020eb2a8935534
Иван Сенькин <plixplox@yandex.ru>
2022-03-14 20:37:14 +0300

    First merge

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class Comments(models.Model):
+    '''Модель комментариев'''
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review = models.ForeignKey(
+        Reviews, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
