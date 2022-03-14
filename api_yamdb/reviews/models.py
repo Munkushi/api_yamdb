5b1f431805446460b300680ceaabdb948b765c16
Антон Николаев <anton.nikolaev1@gmail.com>
2022-03-14 20:20:35 +0300

    Init

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,63 @@
+import datetime as dt
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from .validators import username_validation
+
+
+class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = (
+        (USER, USER),
+        (MODERATOR, MODERATOR),
+        (ADMIN, ADMIN),
+    )
+    username = models.CharField(
+        max_length=25,
+        unique=True,
+        blank=False,
+        null=False,
+        validators=[username_validation],
+        verbose_name='Псевдоним'
+    )
+    email = models.EmailField(
+        max_length=50,
+        unique=True,
+        verbose_name='Адрес почты'
+    )
+    bio = models.TextField(
+        max_length=500,
+        blank=True,
+        verbose_name='Биография'
+    )
+    role = models.CharField(
+        choices=ROLE_CHOICES,
+        default=USER,
+        max_length=15,
+        verbose_name='Роль'
+    )
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name='Имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name='Фамилия'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    @property
+    def is_admin(self):
+        return self.is_staff or self.role == User.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == User.MODERATOR
