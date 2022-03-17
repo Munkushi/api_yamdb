ce519e703be2ddca1aad73be732fbe7d3f802ad2
Иван Сенькин <plixplox@yandex.ru>
2022-03-17 19:19:03 +0300

    urls improvement

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,22 +6,22 @@ from .views import (CommentsViewSet, ReviewViewSet, GenresViewSet,
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(
-    r'titles/(?P<titles_id>\d*)/reviews/',
+    r'titles/(?P<titles_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
 )
 router_v1.register(
-    r'titles/(?P<titles_id>\d*)/reviews/(?P<review_id>\d*)/comments/',
+    r'titles/(?P<titles_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentsViewSet,
     basename='comments',
 )
-router_v1.register("genres/", GenresViewSet)
-router_v1.register("titles/", TitlesViewSet)
-router_v1.register("categories/", CategoriesViewSet)
-router_v1.register("users/", UsersViewSet, basename="users")
+router_v1.register("genres", GenresViewSet)
+router_v1.register("titles", TitlesViewSet)
+router_v1.register("categories", CategoriesViewSet)
+router_v1.register("users", UsersViewSet, basename="users")
 
 urlpatterns = [
-    path('v1', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
     path('v1/auth/signup/', APISignup.as_view(), name='signup'),
 ]
